6ec88d987de5e5b6464bd982a0afe55000048158
Vitalie Melnic <vmwhoami@gmail.com>
2021-06-16 16:40:44 +0200

    Implement the basic filter functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Layout from '../components/layout';
 
 const Home = () => {
+  const [search, setSearch] = useState('');
   const data = useSelector(state => state.firebaseData.data?.data);
   const theKey = () => Math.random().toString(36).slice(2, 7);
+  const handleChange = e => {
+    const { value } = e.target;
+    setSearch(value);
+  };
   return (
     <Layout>
-      {data && data.filter(el => el.toLowerCase().includes('ett'))
+      <form>
+        <input type="text" name="search" value={search} onChange={handleChange} />
+      </form>
+
+      {data && data.filter(el => el.toLowerCase().includes(search.toLowerCase()))
         .map(element => <h2 key={theKey()}>{element}</h2>)}
     </Layout>
   );
